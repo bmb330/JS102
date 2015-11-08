7187c07e6077ded23ee435d3002751e6f37ca56e
Brandon Braun <brandon.braun@gmail.com>
2015-11-08 10:40:20 -0500

    complete objects exercise

--- scripts.js
+++ scripts.js
@@ -1,3 +1,20 @@
 //Use this file to implement Part One of your project
 
+var animal = {};
 
+animal.username = 'Camel';
+animal['tagline'] = 'Hump Day!!!';
+animal['noises'] = [];
+
+var loop_count = 0
+
+for(var key in animal) {
+  loop_count++;
+  
+  if (key === 'username') {
+    console.log('Hi my name is ' + animal[key]);
+  }
+  if (key === 'tagline') {
+    console.log('I like to say ' + animal[key]);
+  }
+};
