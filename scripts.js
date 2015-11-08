9228c2fc48d42a2924eaacf0d19e544749cbd19f
Brandon Braun <brandon.braun@gmail.com>
2015-11-08 15:03:50 -0500

    complete functions exercises

--- scripts.js
+++ scripts.js
@@ -46,3 +46,94 @@ animals.push({ username: 'Duck', tagline: 'Quack', noises: noiseArray });
 animals.push({ username: 'Bird', tagline: 'Chirp', noises: ['chirp', 'tweet'] });
 
 console.log(animals.length);
+
+// Constructors/Looping
+
+function AnimalMaker(name) {
+  return {
+    speak: function () {
+      console.log("my name is ", name);
+    },
+    name: name
+  };
+};
+
+var animalNames = ['Sheep', 'Liger', 'Big Bird'];
+
+var farm = [];
+
+for (var i = 0; i < animalNames.length; i++) {
+  farm.push(AnimalMaker(animalNames[i]));
+}
+
+// Part Three
+
+function AnimalTestUser(username) {
+  var otherArgs = [];
+  if (arguments.length > 1) {
+    for (var i = 1; i < arguments.length; i++) {
+      otherArgs.push(arguments[i]);
+    }
+  }
+
+  return {
+    username: username,
+    otherArgs: otherArgs
+  };
+};
+
+var testSheep = AnimalTestUser('CottonBall', { 'loves dancing': true }, [1,2,3]);
+console.log(testSheep);
+
+function AnimalCreator(username, species, tagline, noises) {
+  return {
+    username: username,
+    species: species,
+    tagline: tagline,
+    noises: noises,
+    friends: []
+  };
+}
+
+var sheep = AnimalCreator('Cloud', 'sheep', 'You can count on me!', ['baahhh', 'arrgg', 'chewchewchew']);
+console.log(sheep);
+
+function addFriend(animal, friend) {
+  animal.friends.push(friend.username);
+}
+
+addFriend(sheep, animals[3]);
+console.log(sheep);
+
+addFriend(sheep, animals[1]);
+console.log(sheep);
+
+var myFarm = [
+  AnimalCreator('Bird', 'bird', 'Thats all folks', ['tweet']),
+  AnimalCreator('Cow', 'cow', 'Grass, Grass, Grass', ['moo']),
+  AnimalCreator('Goose', 'bird', 'Layin Eggs', ['quak'])
+]
+
+addFriend(myFarm[0], myFarm[1]);
+addFriend(myFarm[1], myFarm[0]);
+addFriend(myFarm[2], myFarm[1]);
+
+console.log(myFarm);
+
+function addMatchesArray(farm) {
+  for (var i = 0; i < farm.length; i++) {
+    farm[i].matches = [];
+  }
+}
+
+addMatchesArray(myFarm); 
+console.log(myFarm[0]);
+
+function giveMatches(farm) {
+  for (var i = 0; i < farm.length; i++) {
+    farm[i].matches.push(farm[i].friends[0]);
+  }
+}
+
+giveMatches(myFarm); 
+console.log(myFarm[0]);
