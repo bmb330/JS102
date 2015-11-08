dc785e2683441b4c36a92f07fbba1d0fe1f84fbd
Brandon Braun <brandon.braun@gmail.com>
2015-11-08 11:17:07 -0500

    complete arrays exercise

--- scripts.js
+++ scripts.js
@@ -18,3 +18,31 @@ for(var key in animal) {
     console.log('I like to say ' + animal[key]);
   }
 };
+
+// Part Two
+
+var noiseArray = ['yak'];
+
+noiseArray.unshift('wak');
+noiseArray.push('qak');
+
+noiseArray[3] = 'nak';
+
+console.log(noiseArray.length);
+console.log(noiseArray.length - 1);
+console.log(noiseArray);
+
+animal.noises = noiseArray;
+
+animals = [];
+
+animals.push(animal);
+
+var quackers = animal;
+
+animals.push(quackers);
+
+animals.push({ username: 'Duck', tagline: 'Quack', noises: noiseArray });
+animals.push({ username: 'Bird', tagline: 'Chirp', noises: ['chirp', 'tweet'] });
+
+console.log(animals.length);
